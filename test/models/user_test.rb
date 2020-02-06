ee96118b5f027e38bb8a837776dcd5d7881f2dd4
konyan <devkonyan@gmail.com>
2020-02-07 03:15:21 +0630

    done 6.25 uniquess valadation

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -56,4 +56,11 @@ class UserTest < ActiveSupport::TestCase
     @user.save
     assert_not duplicate_user.valid?
   end
+
+  test "email address should be saved as lower-case" do
+    mixed_case_email = "Foo@ExAMPLE.CoM"
+    @user.email = mixed_case_email
+    @user.save
+    assert_equal mixed_case_email.downcase, @user.reload.email
+  end
 end
