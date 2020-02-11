cf826921a5b37f2f5cf8c2be918c031c71b9aec7
konyan <devkonyan@gmail.com>
2020-02-12 01:05:23 +0630

    finish user sign up

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   get '/signup', to:'users#new'
+  post '/signup',  to: 'users#create'
+
   root 'static_pages#home'
   get '/help',  to: 'static_pages#help'
   get '/about', to: 'static_pages#about'
   get '/contact', to: 'static_pages#contact'
+  resources :users
 end
