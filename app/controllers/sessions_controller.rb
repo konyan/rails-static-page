1cb3bb84892578f60c2451db7c0ef9adbe19b305
konyan <devkonyan@gmail.com>
2020-02-26 03:18:17 +0630

    add logout route and logout seesion at forum

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,7 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
       log_in user
-      redirect_to current_user
+      redirect_to user
     else
       flash.now[:danger] = 'invalid email/password combination'
       render 'new'
@@ -14,7 +14,8 @@ class SessionsController < ApplicationController
   end
 
   def destroy
+    log_out
+    redirect_to root_url
   end
-
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,6 @@ Rails.application.routes.draw do
 
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
-  delete '/logout', to: 'sessions#destory'
+  delete '/logout', to: 'sessions#destroy'
   resources :users
 end
